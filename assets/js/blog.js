b64d61e373800ffbdf119c6d64bff40bf45c55f3
delucianina <delucianina@gmail.com>
2024-10-02 20:13:18 -0400

    finished

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -1,5 +1,6 @@
 // TODO: Create a variable that selects the main element, and a variable that selects the back button element
 const main = document.querySelector('#main-tag');
+const backBtn = document.querySelector('#back');
 
 // TODO: Create a function that builds an element and appends it to the DOM
 function showPosts() {
@@ -38,7 +39,7 @@ function showPosts() {
 
 // TODO: Create a function that handles the case where there are no blog posts to display
 function errorDisplay() {
-
+    main.innerHTML = 'No Blog posts yet...';
 };
 
 
@@ -65,3 +66,6 @@ renderBlogList();
 // TODO: Call the `renderBlogList` function
 
 // TODO: Redirect to the home page using the `redirectPage` function found in logic.js when the back button is clicked
+backBtn.addEventListener('click', () => {
+    redirectPage('./index.html');
+});
